183066c3b6fb8e45bb9195dc1bbfcb614193eeb2
Althea Algaba <altheaalgaba16@gmail.com>
2024-11-04 21:33:11 +0800

    adding aot

---
+++ AOT/human.js
@@ -0,0 +1,42 @@
+var characterContainer = document.getElementById("characterContainer");
+
+const loadHuman = async () =>{
+    const humanResponse = await fetch (`https://api.attackontitanapi.com/characters/`);
+    const human = await humanResponse.json();
+
+    for (var i = 1; i <= human.info.count; i++){
+        const characterResponse = await fetch (`https://api.attackontitanapi.com/characters/` + i);
+        const character = await characterResponse.json();
+
+  
+
+        characterContainer.innerHTML += 
+        `
+        <div class="col-12 col-sm-12 col-md-4 col-xl-3 profile">
+                <!-- Display 1 -->
+                <a href="viewPage.html"><button class="btn" style="background-color: transparent; border-radius: 30px;"><div class="card align-items-center profileCard mx-auto"
+                    style="height: 287px; width: 237px; background-color: #4F4F4F;">
+
+                    <!-- image -->
+                    <div class="row charImage">
+                        <div class="container p-3" style="height: 213px; width: 237px;"> <img src="${character.img}"
+                                class="img-fluid image" style="border-radius: 30px;" > </div>
+                    </div>
+
+                    <!-- nametag -->
+                    <div class="row" style="height: 74px; width: 237px;">
+                        <div class="nameTag" style="background-color: white;">
+                            <p><b>${character.name}</b></p>
+                        </div>
+                    </div>
+                </div></button> </a>
+        </div>
+        `;
+
+
+    }
+    
+
+}
+
+loadHuman();
---
+++ AOT/script.js
@@ -0,0 +1,46 @@
+var searchResultContainer = document.getElementById("searchResultContainer");
+
+const loadData = async (searchTerm) => {
+
+
+    try{
+        const dataResponse = await fetch (`https://api.attackontitanapi.com/characters/?name=${searchTerm}`);
+        
+            const data = await dataResponse.json();
+            searchResultContainer.innerHTML =`
+            <div class="col-12" style="height: 200px;">         
+                <div class="card mx-auto align-items-center" style="background-color: #a9a7a7; max-width: 554px;">
+                    <div class="row">
+                        <div class="container"> <img src="${data.results[0].img}"> </div>
+                    </div>
+        
+                            
+                    <div class="row">
+                        <div class="container p-5" style=" height: 74; max-width: 440px;"> <b>${data.results[0].name}</b> </div>
+                    </div>
+                </div>
+        
+            </div>
+            `;
+
+        
+
+        }
+    catch (error){
+        searchResultContainer.innerHTML =`
+        <div class="alert alert-danger mx-auto p-5" style="max-width:500px">
+            <strong>Error 404[Character Not Found!]</strong>
+        </div>
+        `
+    }
+
+    
+    
+}
+
+function searchData(){
+    var searchTerm = document.getElementById("searchBar").value;
+    loadData(searchTerm)
+
+
+}
